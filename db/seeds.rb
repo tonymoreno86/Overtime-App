261dcc32392021107993b8a69e8ba49350291cb7
Anthony Moreno <tonymoreno86@gmail.com>
2018-05-24 20:57:39 -0700

    Updated seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,24 +1,9 @@
-User.create(email: "test@test.com",
-                    password: "asdfasdf",
-                    password_confirmation: "asdfasdf",
-                    first_name: "Jon",
-                    last_name: "Snow",
-                     )
+@user = User.create(email: "test@test.com", password: "asdfasdf", password_confirmation: "asdfasdf", first_name: "Tony", last_name: "Moreno")
 
-puts "1 employee created"
-
-AdminUser.create(email: "admin@test.com",
-                  password: "asdfasdf",
-                  password_confirmation: "asdfasdf",
-                  first_name: "Admin",
-                  last_name: "Name",
-                  )
-
-puts "1 Admin user created"
-
+puts '1 user created'
 
 100.times do |post|
-  Post.create!(date: Date.today, rationale: "#{post} rationale content")
+  Post.create!(date: Date.today, rationale: "#{post} rationale content", user_id: @user.id)
 end
 
 puts "100 Posts have been created"
