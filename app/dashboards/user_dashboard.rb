e336dd4e41c3a1664138d1736dd2ae0a5eff83b1
Anthony Moreno <tonymoreno86@gmail.com>
2018-06-15 14:57:27 -0700

    Integrated phone validations

--- app/dashboards/user_dashboard.rb
+++ app/dashboards/user_dashboard.rb
@@ -16,7 +16,7 @@ class UserDashboard < Administrate::BaseDashboard
     created_at: Field::DateTime.with_options(searchable: false),
     updated_at: Field::DateTime.with_options(searchable: false),
     type: Field::String.with_options(searchable: false),
-    phone: Field::String.with_options(searchable: false)
+    phone: Field::String.with_options(searchable: false),
   }.freeze
 
   COLLECTION_ATTRIBUTES = [
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -8,7 +8,9 @@ RSpec.describe User, type: :model do
     it "can be created" do
       expect(@user).to be_valid
     end
+  end
 
+  describe 'validatios' do
     it "cannot be created without first_name" do
       @user.first_name = nil
       expect(@user).to_not be_valid
@@ -21,6 +23,16 @@ RSpec.describe User, type: :model do
 
     it "cannot be created without phone number" do
       @user.phone = nil
+      expect(@user).to_not be_valid
+    end
+
+    it 'requires the phone attr to only contain integers' do
+      @user.phone = 'mygreatstr'
+      expect(@user).to_not be_valid
+    end
+
+    it 'requires the phone attr to only have 10 characters' do 
+      @user.phone = '12345678901'
       expect(@user).to_not be_valid
     end
   end
