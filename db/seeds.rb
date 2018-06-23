cfd94de36be6d5a1faadd3ef1306a474f65595c3
Anthony Moreno <tonymoreno86@gmail.com>
2018-06-22 21:03:54 -0700

    Brought in live data for admin review page

--- db/seeds.rb
+++ db/seeds.rb
@@ -17,7 +17,7 @@ AdminUser.create(email: "admin@user.com",
 puts '1 Admin user created'
 
 100.times do |post|
-  Post.create!(date: Date.today, rationale: "#{post} rationale content", user_id: @user.id, overtime_request: 2.5)
+  Post.create!(date: Date.today, rationale: "#{post} rationale content Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.", user_id: @user.id, overtime_request: 2.5)
 end
 
 puts "100 Posts have been created"
