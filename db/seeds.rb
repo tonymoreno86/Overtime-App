c055bcd5560b52f89b59e5b9753bf4cfb7edcf4a
Anthony Moreno <tonymoreno86@gmail.com>
2018-07-02 17:40:01 -0700

    Built out rake task for sending sms messages and STI for employees

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,11 +1,11 @@
-@user = User.create(email: "test@test.com", 
+@employee = Employee.create(email: "test@test.com", 
                     password: "asdfasdf", 
                     password_confirmation: "asdfasdf", 
                     first_name: "Tony", 
                     last_name: "Moreno",
-                    phone: '4245677608')
+                    phone: '3238293298')
 
-puts '1 user created'
+puts '1 employee created'
 
 AdminUser.create(email: "admin@user.com", 
                   password: "benzc230", 
@@ -16,14 +16,14 @@ AdminUser.create(email: "admin@user.com",
 
 puts '1 Admin user created'
 
-AuditLog.create!(user_id: @user.id, status: 0, start_date: (Date.today - 6.days))
-AuditLog.create!(user_id: @user.id, status: 0, start_date: (Date.today - 13.days))
-AuditLog.create!(user_id: @user.id, status: 0, start_date: (Date.today - 20.days))
+AuditLog.create!(user_id: @employee.id, status: 0, start_date: (Date.today - 6.days))
+AuditLog.create!(user_id: @employee.id, status: 0, start_date: (Date.today - 13.days))
+AuditLog.create!(user_id: @employee.id, status: 0, start_date: (Date.today - 20.days))
 
 puts "3 audit logs have been created"
 
 100.times do |post|
-  Post.create!(date: Date.today, rationale: "#{post} rationale content Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.", user_id: @user.id, overtime_request: 2.5)
+  Post.create!(date: Date.today, rationale: "#{post} rationale content Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.", user_id: @employee.id, overtime_request: 2.5)
 end
 
 puts "100 Posts have been created"
